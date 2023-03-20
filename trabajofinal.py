adc9fe28a8f6cde1da9d4ac5e7b44d7c42ccb9a5
cristian <you@example.com>
2023-03-19 21:04:52 -0300

    funcionando

--- trabajofinal.py
+++ trabajofinal.py
@@ -1,141 +1,24 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.messagebox import *
-from tkinter.colorchooser import askcolor
 import sqlite3
 import re
 
-# BASE SQLITE--------------------------------------------------------------------------
 
-
-def base():
-    con = sqlite3.connect("base_ejemplo.db")
-    return con
-
-
-def crear_tabla():
-    con = base()
-    cursor = con.cursor()
-    sql = """CREATE TABLE clientes (cuenta INTERGER, reparto INTERGER, numero_de_cliente INTERGER PRIMARY KEY, razonsocial VARCHAR, direccion VARCHAR, localidad VARCHAR)"""
-    cursor.execute(sql)
-    con.commit()
-
-
-try:
-    base()
-    crear_tabla()
-except:
-    print("Hay un error")
-
-
-def funcion_alta(
-    cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad, tree
-):
-    cuenta = valor_cuenta.get()
-    reparto = valor_reparto.get()
-    numero_de_cliente = valor_cliente.get()
-    razonsocial = valor_razon.get()
-    direccion = valor_direccion.get()
-    localidad = valor_localidad.get()
-    regex = valor_razon.get()
-    expresion = "[a-zA-Z0-9_]"
-    if re.match(expresion, regex):
-        Label(mensaje, text="Registro valido", font="Courier, 10", fg="blue2").place(
-            x=280, y=100
-        )
-        con = base()
-        cursor = con.cursor()
-        data = (cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad)
-        sql = "INSERT INTO clientes(cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad) VALUES(?, ?, ?, ?, ?, ?)"
-        cursor.execute(sql, data)
-        con.commit()
-        tree.insert(
-            "",
-            "end",
-            values=(
-                valor_cuenta.get(),
-                valor_reparto.get(),
-                valor_cliente.get(),
-                valor_razon.get(),
-                valor_direccion.get(),
-                valor_localidad.get(),
-            ),
-        )
-    else:
-        Label(
-            mensaje,
-            text="Invalido. Registro solo alfanumerico",
-            font="Courier, 10",
-            fg="red2",
-        ).place(x=280, y=100)
-    actualizar(tree)
-    limpiar_registro()
-
-
-def limpiar_registro():
-    valor_cuenta.set(""), valor_reparto.set(""), valor_cliente.set(""), valor_razon.set(
-        ""
-    ), valor_direccion.set(""), valor_localidad.set("")
-
-
-def funcion_borrar(tree):
-    if askyesno("Base Clientes", "Desea eliminar el registro?"):
-        cliente = tree.selection()
-        print(cliente)
-        item = tree.item(cliente)
-        print(item)
-        print(item["text"])
-        mi_id = item["text"]
-        con = base()
-        cursor = con.cursor()
-        data = (mi_id,)
-        sql = "DELETE FROM clientes WHERE cuenta = ?;"
-        cursor.execute(sql, data)
-        con.commit()
-        tree.delete(cliente)
-        showinfo("Base Clientes", "Registro eliminado")
-    else:
-        showinfo("No", "No se eliminara el registro")
-
-
-def actualizar(tree):
-    records = tree.get_children()
-    for element in records:
-        tree.delete(element)
-        sql = "SELECT * FROM clientes ORDER BY cuenta ASC"
-    con = base()
-    cursor = con.cursor()
-    datos = cursor.execute(sql)
-    resultado = datos.fetchall()
-    for fila in resultado:
-        print(fila)
-        tree.insert(
-            "",
-            "end",
-            text=fila[0],
-            values=(fila[1], fila[2], fila[3], fila[4], fila[5]),
-        )
-
-
-def funcion_modificar(tree):
-    if askyesno("Base Clientes", "Desea modificar el registro?"):
-        cliente = tree.selection()
-        item = tree.item(cliente)
-        mi_id = item["text"]
-        con = base()
-        cursor = con.cursor()
-        sql = f"UPDATE clientes SET cuenta = '{valor_cuenta.get()}', reparto = '{valor_reparto.get()}', numero_de_cliente = '{valor_cliente.get()}', razonsocial = '{valor_razon.get()}', direccion = '{valor_direccion.get()}', localidad = '{valor_localidad.get()}' WHERE cuenta = '?';"
-        cursor.execute(sql)
-        con.commit()
-        funcion_alta(
-            cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad, tree
-        )
-        showinfo("Base Clientes", "Registro modificado")
-    else:
-        showinfo("No", "No se modificara el registro")
-
+# -------------------------------------------------------------------------------
+# AUTOR CALIQUIS CRISTIAN
+# ------------------------------------------------------------------------------
 
 aplicacion = Tk()
+
+
+valor_cuenta = IntVar()
+valor_reparto = IntVar()
+valor_cliente = IntVar()
+valor_razon = StringVar()
+valor_direccion = StringVar()
+valor_localidad = StringVar()
+
 
 titulo = Label(
     aplicacion,
@@ -147,6 +30,7 @@ titulo = Label(
     font=("bold"),
 )
 titulo.grid(row=0, column=0, columnspan=7, padx=1, pady=1, sticky=W + E)
+
 
 cuenta = Label(aplicacion, text="Cuenta :", fg="black", anchor="center")
 cuenta.grid(row=1, column=0, sticky=W)
@@ -166,15 +50,6 @@ direccion.grid(row=5, column=0, sticky=W)
 localidad = Label(aplicacion, text="Localidad :", fg="black", anchor="center")
 localidad.grid(row=6, column=0, sticky=W)
 
-mensaje = ()
-valor_cuenta = IntVar()
-valor_reparto = IntVar()
-valor_cliente = IntVar()
-valor_razon = StringVar()
-valor_direccion = StringVar()
-valor_localidad = StringVar()
-
-
 entry_cuenta = Entry(aplicacion, textvariable=valor_cuenta, width=15)
 entry_cuenta.grid(row=1, column=0)
 entry_reparto = Entry(aplicacion, textvariable=valor_reparto, width=15)
@@ -187,6 +62,171 @@ entry_direccion = Entry(aplicacion, textvariable=valor_direccion, width=30)
 entry_direccion.grid(row=5, column=0)
 entry_localidad = Entry(aplicacion, textvariable=valor_localidad, width=30)
 entry_localidad.grid(row=6, column=0)
+entrybusqueda = ttk.Entry(aplicacion)
+entrybusqueda.grid(row=9, column=0, padx=55, pady=8, ipady=3, ipadx=60)
+
+
+# BASE SQLITE--------------------------------------------------------------------------
+
+
+def base():
+    con = sqlite3.connect("base_ejemplo.db")
+    return con
+
+
+def crear_tabla():
+    con = base()
+    cursor = con.cursor()
+    sql = """CREATE TABLE clientes (cuenta INTERGER, reparto INTERGER, numero_de_cliente INTERGER PRIMARY KEY, razonsocial VARCHAR, direccion VARCHAR, localidad VARCHAR)"""
+    cursor.execute(sql)
+    con.commit()
+
+
+# Funciones CRUD-------------------------------------------------------------------------
+
+
+def actualizar(tree):
+    records = tree.get_children()
+    for element in records:
+        tree.delete(element)
+    sql = "SELECT * FROM clientes ORDER BY cuenta ASC"
+    con = base()
+    cursor = con.cursor()
+    datos = cursor.execute(sql)
+    resultado = datos.fetchall()
+    for fila in resultado:
+        print(fila)
+        tree.insert(
+            "",
+            "end",
+            text=fila[0],
+            values=(fila[1], fila[2], fila[3], fila[4], fila[5]),
+        )
+    limpiar_registro()
+
+
+def funcion_alta(
+    cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad, tree
+):
+    if askquestion("Base Clientes", "Desea crear el registro"):
+        cuenta = valor_cuenta.get()
+        reparto = valor_reparto.get()
+        numero_de_cliente = valor_cliente.get()
+        razonsocial = valor_razon.get()
+        direccion = valor_direccion.get()
+        localidad = valor_localidad.get()
+        regex = valor_razon.get()
+        expresion = "[a-zA-ZÀ-ÿ\u00f1\u00d1(0-9]"
+        if re.match(expresion, regex):
+            Label(
+                aplicacion, text="Registro valido", font="Courier, 10", fg="blue2"
+            ).place(x=280, y=100)
+            con = base()
+            cursor = con.cursor()
+            data = (
+                cuenta,
+                reparto,
+                numero_de_cliente,
+                razonsocial,
+                direccion,
+                localidad,
+            )
+            sql = "INSERT INTO clientes(cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad) VALUES(?, ?, ?, ?, ?, ?)"
+            cursor.execute(sql, data)
+            con.commit()
+            tree.insert(
+                "",
+                "end",
+                values=(
+                    valor_cuenta.get(),
+                    valor_reparto.get(),
+                    valor_cliente.get(),
+                    valor_razon.get(),
+                    valor_direccion.get(),
+                    valor_localidad.get(),
+                ),
+            )
+            showinfo("Base Clientes", "Registro creado")
+        else:
+            Label(
+                aplicacion,
+                text="Invalido. Registro solo alfanumerico",
+                font="Courier, 10",
+                fg="red2",
+            ).place(x=280, y=100)
+            showwarning("Base Clientes", "No se pudo crear el registro")
+    else:
+        showwarning("Base Clientes", "No se pudo crear el registro")
+        actualizar(tree)
+        limpiar_registro()
+    limpiar_registro()
+
+
+def limpiar_registro():
+    valor_cuenta.set("0"), valor_reparto.set("0"), valor_cliente.set(
+        "0"
+    ), valor_razon.set(""), valor_direccion.set(""), valor_localidad.set("")
+
+
+def funcion_borrar(tree):
+    if askquestion("Base Clientes", "Desea eliminar el registro"):
+        Label(
+            aplicacion, text="Registro Eliminado", font="Courier, 15", fg="blue2"
+        ).place(x=400, y=220)
+        cliente = tree.selection()
+        item = tree.item(cliente)
+        print(item)
+        print(item["text"])
+        mi_id = item["text"]
+        con = base()
+        cursor = con.cursor()
+        data = (mi_id,)
+        tree.delete(cliente)
+        sql = "DELETE FROM clientes WHERE cuenta = ?;"
+        cursor.execute(sql, data)
+        con.commit()
+        limpiar_registro()
+    else:
+        Label(
+            aplicacion, text="Registro No Eliminado", font="Courier, 15", fg="blue2"
+        ).place(x=400, y=220)
+
+
+def funcion_modificar(tree):
+    if askyesno("Base Clientes", "Desea modificar el registro?"):
+        cliente = tree.selection()
+        item = tree.item(cliente)
+        mi_id = item["text"]
+        con = base()
+        cursor = con.cursor()
+        sql = f"UPDATE clientes SET cuenta = '{valor_cuenta.get()}', reparto = '{valor_reparto.get()}', numero_de_cliente = '{valor_cliente.get()}', razonsocial = '{valor_razon.get()}', direccion = '{valor_direccion.get()}', localidad = '{valor_localidad.get()}' WHERE cuenta = '{mi_id}';"
+        cursor.execute(sql)
+        con.commit()
+        actualizar(tree)
+        showinfo("Base Clientes", "Registro modificado")
+    else:
+        showinfo(
+            "Base Clientes", "Debe seleccionar un registro y completar todos los campos"
+        )
+
+
+def funcion_buscar(tree):
+    records = tree.get_children()
+    for element in records:
+        tree.delete(element)
+    sql = f"SELECT * FROM clientes WHERE cuenta LIKE '%{entrybusqueda.get()}%' OR reparto LIKE '%{entrybusqueda.get()}%' OR numero_de_cliente LIKE '%{entrybusqueda.get()}%' OR razonsocial LIKE '%{entrybusqueda.get()}%' OR direccion LIKE '%{entrybusqueda.get()}%' OR localidad LIKE '%{entrybusqueda.get()}%' "
+    con = base()
+    cursor = con.cursor()
+    datos = cursor.execute(sql)
+    resultado = datos.fetchall()
+    for fila in resultado:
+        print(fila)
+        tree.insert(
+            "",
+            "end",
+            text=fila[0],
+            values=(fila[1], fila[2], fila[3], fila[4], fila[5]),
+        )
 
 
 # -------------------------------------------------
@@ -196,9 +236,6 @@ entry_localidad.grid(row=6, column=0)
 tree = ttk.Treeview(aplicacion)
 
 aplicacion.geometry("1060x700")
-
-entrybusqueda = ttk.Entry(aplicacion)
-entrybusqueda.grid(row=9, column=0, padx=5, pady=5, ipady=3, ipadx=60)
 
 s = ttk.Style()
 s.theme_use("clam")
@@ -236,7 +273,13 @@ boton_guardar = Button(
     padx=71,
     pady=3,
     command=lambda: funcion_alta(
-        cuenta, reparto, numero_de_cliente, razonsocial, direccion, localidad, tree
+        cuenta,
+        reparto,
+        numero_de_cliente,
+        razonsocial,
+        direccion,
+        localidad,
+        tree,
     ),
 )
 boton_guardar.grid(row=3, column=2)
@@ -280,6 +323,16 @@ boton_actualizar = Button(
     command=lambda: actualizar(tree),
 )
 boton_actualizar.grid(row=6, column=2)
+
+
+boton_buscar = Button(
+    aplicacion,
+    text="Buscar",
+    bg="Grey49",
+    fg="white",
+    command=lambda: funcion_buscar(tree),
+)
+boton_buscar.grid(row=9, column=0, padx=15, pady=8, ipady=3, ipadx=3, sticky=W)
 
 
 aplicacion.mainloop()
